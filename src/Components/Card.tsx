ac6b5fab42df57636cb8f71c335b8bc367313273
aayushi Kambariya <aayushi.kambriya5@gmail.com>
2024-08-30 01:33:14 +0530

    chores: Setup with basic UI features

---
+++ src/Components/Card.tsx
@@ -0,0 +1,54 @@
+import { useSortable } from '@dnd-kit/sortable'
+import {CSS } from '@dnd-kit/utilities'
+import React, { useEffect } from 'react'
+
+type CardProps = {
+    id: string
+    type?: string
+    title: string
+    position: number
+    imgUrl: string
+}
+
+const Card: React.FC<CardProps> = ({id, title, imgUrl}) => {
+
+    const [showFullScreen, setShowFullScreen] = React.useState<boolean>(false)
+    const {setNodeRef, attributes, listeners, transform, transition}  =  useSortable({id})
+
+    const handleCardClick = () => {
+        setShowFullScreen(true)
+    }
+
+    // Hide fullscreen image on escape
+    useEffect(() => {
+        const handleEsc = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                setShowFullScreen(false)
+            }
+        };
+        window.addEventListener('keydown', handleEsc);
+
+        return () => {
+          window.removeEventListener('keydown', handleEsc);
+        };
+      }, []);
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform),
+    }
+
+    return (
+        <>
+            <div ref={setNodeRef} onClick={handleCardClick} {...attributes} {...listeners} style={style} className='shadow-lg p-2 cursor-pointer' tabIndex={0} >
+                <div>{title}</div>
+                <img src={imgUrl}/>
+            </div>
+            {showFullScreen && <div className='h-screen w-screen fixed top-0 left-0' tabIndex={0}><img src={imgUrl}/></div>}
+        </>
+       
+
+    )
+}
+
+export default Card
---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import Card from '../Components/Card'
+import jsonData from '../assets/data.json'
+import { DndContext, DragEndEvent, KeyboardSensor, PointerSensor, TouchSensor, closestCorners, useSensor, useSensors } from '@dnd-kit/core'
+import { SortableContext, arrayMove, horizontalListSortingStrategy, sortableKeyboardCoordinates } from '@dnd-kit/sortable'
+
+
+const HomePage = () => {
+    
+    const [data, setData] = useState(jsonData)
+
+
+    const getDataId = (id:string) => data?.findIndex(item => item.id === id)
+
+    const handleDragEnd = (event: DragEndEvent ) => {
+        const {active, over} = event
+        if(active?.id === over?.id) return 
+        setData((data) => {
+            const original = getDataId(active?.id)
+            const newPosition = getDataId(over?.id)
+            console.log(active, over)
+            return arrayMove(data, original, newPosition)
+        })
+    }
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor,{
+            coordinateGetter: sortableKeyboardCoordinates
+        }),
+        useSensor(TouchSensor)
+    )   
+
+    return (
+        <DndContext sensors={sensors} collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+        <div className='grid grid-cols-3 gap-3'>
+            <SortableContext  items={data} strategy={horizontalListSortingStrategy}>
+            {data?.map(({id, ...props}) => (
+               <Card {...props} id={id} key={id}/>
+            ))}
+            </SortableContext>
+        </div>
+        </DndContext>
+    )
+}
+
+export default HomePage
+
+
