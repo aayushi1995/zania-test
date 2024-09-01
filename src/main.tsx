77fbd3400f5f22b1aa985b8628988e38aafb3176
aayushi Kambariya <aayushi.kambriya5@gmail.com>
2024-09-02 02:22:54 +0530

    fix: removed production constraints

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
  
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
  
   const { worker } = await import('./mocks/browser')
  
